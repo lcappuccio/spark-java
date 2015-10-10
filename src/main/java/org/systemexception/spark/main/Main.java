3af1da321c67817fbadede08a3f6369faf9b9e19
Leonardo Cappuccio <leo@systemexception.org>
2015-10-11 01:27:26 +0200

    add date route

--- src/main/java/org/systemexception/spark/main/Main.java
+++ src/main/java/org/systemexception/spark/main/Main.java
@@ -1,5 +1,7 @@
 package org.systemexception.spark.main;
 
+import java.time.LocalDateTime;
+
 import static spark.Spark.get;
 
 /**
@@ -10,5 +12,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		get("/hello", (req, res) -> "Hello World");
+
+
+		get("/time", (request, response) -> getDate());
+	}
+
+	private static String getDate() {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		return localDateTime.toString();
 	}
 }
